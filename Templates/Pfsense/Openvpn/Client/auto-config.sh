7d0c9ec02393c30facd9ef8906274c645fd6c097
victorfmaraujo <victorfmaraujo@gmail.com>
2018-09-10 17:47:11 -0300

    download dos scripts

--- Templates/Pfsense/Openvpn/Client/auto-config.sh
+++ Templates/Pfsense/Openvpn/Client/auto-config.sh
@@ -1,4 +1,5 @@
 pw groupmod wheel -m zabbix
 mkdir /scripts
-fetch -q -o /scripts/gateway.php https://raw.githubusercontent.com/lndgoncalves/zabbix-pfsense-gateway/master/gateway.php
+fetch -q -o https://raw.githubusercontent.com/victorfmaraujo/zabbix/master/Templates/Pfsense/Openvpn/Client/openvpn.clients.data.php
+fetch -q -o https://raw.githubusercontent.com/victorfmaraujo/zabbix/master/Templates/Pfsense/Openvpn/Client/openvpn.clients.discovery.php
 chmod +x /scripts/gateway.php
