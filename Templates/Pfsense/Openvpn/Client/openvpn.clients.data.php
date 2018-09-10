56b243001ba63cbde22e2d22c4838c9af0a0e4b1
victorfmaraujo <victorfmaraujo@gmail.com>
2018-09-10 17:44:28 -0300

    Primeiro Commit
    adicionados arquivos, scripts e template

---
+++ Templates/Pfsense/Openvpn/Client/openvpn.clients.data.php
@@ -0,0 +1,24 @@
+<?php
+require_once("/etc/inc/openvpn.inc");
+$op1 =  $argv[1];
+$op2 =  $argv[2];
+
+$dados = [];
+
+foreach (openvpn_get_active_clients(true) as $server){
+    if($server['name'] == $op1) {
+        $data = $server[$op2];
+            if ($data == 'up') {
+	            $data = 1;}
+	        elseif ($data == 'down') {
+                    $data = 0;}
+            elseif ($data == 'reconnecting; ping-restart') {
+            		$data = 2;}
+            elseif ($data == 'waiting') {
+                    $data = 3;}                        
+            else {$data = $server[$op2];}
+    }
+        echo $data;
+$data = null;
+}
+?>
