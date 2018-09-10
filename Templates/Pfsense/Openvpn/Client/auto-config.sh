ed115b3114f9a295bc8772914a0757fedd9b277f
victorfmaraujo <victorfmaraujo@gmail.com>
2018-09-10 17:47:59 -0300

    Correção do script

--- Templates/Pfsense/Openvpn/Client/auto-config.sh
+++ Templates/Pfsense/Openvpn/Client/auto-config.sh
@@ -2,4 +2,5 @@ pw groupmod wheel -m zabbix
 mkdir /scripts
 fetch -q -o https://raw.githubusercontent.com/victorfmaraujo/zabbix/master/Templates/Pfsense/Openvpn/Client/openvpn.clients.data.php
 fetch -q -o https://raw.githubusercontent.com/victorfmaraujo/zabbix/master/Templates/Pfsense/Openvpn/Client/openvpn.clients.discovery.php
-chmod +x /scripts/gateway.php
+chmod +x /scripts/openvpn.clients.data.php
+chmod +x /scripts/openvpn.clients.discovery.php
