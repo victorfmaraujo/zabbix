af5edbdf45cd87b9f1300be619dfc449c405a8e5
victorfmaraujo <victorfmaraujo@gmail.com>
2018-09-10 17:58:23 -0300

    ..

--- Templates/Pfsense/Openvpn/Client/auto-config.sh
+++ Templates/Pfsense/Openvpn/Client/auto-config.sh
@@ -1,6 +1,6 @@
 pw groupmod wheel -m zabbix
 mkdir /scripts
-fetch -q -o https://raw.githubusercontent.com/victorfmaraujo/zabbix/master/Templates/Pfsense/Openvpn/Client/openvpn.clients.data.php
-fetch -q -o https://raw.githubusercontent.com/victorfmaraujo/zabbix/master/Templates/Pfsense/Openvpn/Client/openvpn.clients.discovery.php
+fetch -q -o /scripts/openvpn.clients.data.php https://raw.githubusercontent.com/victorfmaraujo/zabbix/master/Templates/Pfsense/Openvpn/Client/openvpn.clients.data.php
+fetch -q -o /scripts/openvpn.clients.discovery.php https://raw.githubusercontent.com/victorfmaraujo/zabbix/master/Templates/Pfsense/Openvpn/Client/openvpn.clients.discovery.php
 chmod +x /scripts/openvpn.clients.data.php
 chmod +x /scripts/openvpn.clients.discovery.php
