e1bbbf38fa6aad86c4fc9f4b2d315a71548233ee
Victor França <victorfmaraujo@gmail.com>
2021-04-07 15:47:25 -0300

    Monitoramento de Jobs do Cloudberry
    primeira versão, monitoramento de jobs do cloudberry

---
+++ Templates/Windows/App-Cloudberry/cloudberry-coleta.ps1
@@ -0,0 +1,62 @@
+﻿Param(
+  [string]$a,
+  [string]$b,
+  [string]$c
+  # $a seleciona se é job ou storage
+  # $b ID do JOB
+  # $c ID do Storage
+)
+if ($b -eq $null) {
+    $b = "nothing"
+    }
+
+function get-CBBackupStatusDataJob {
+set-location "C:\Program Files\CloudBerryLab\CloudBerry Backup"
+    $jobdiscovery="C:\Zabbix\discovery.job.txt"
+    $data=Get-Content -Path $jobdiscovery
+    $count = ($data -match ('^Name:') -replace ('^Name: ','')).count
+    $JobList=$data -match ('^Name:') -replace ('^Name: ','')  
+    $JobIds=$data -match ('^ID:') -replace ('^ID: ','')  
+    $JobResult=$data -match ('^Last Result:') -replace ('^Last Result: ','')
+    #Transforma a string do resultado em Decimal
+    $JobResult=$JobResult -replace ('Never started','0') -replace ('Falha','1') -replace ('Alerta','2') -replace ('Sucesso','3')
+    $List = New-Object -TypeName System.Collections.ArrayList
+    for($i=0; $i -lt $count; $i++){
+        $Obj = [PSCustomObject]@{
+            "{#JOBID}" = $JobIds[$i]
+            "{#JOBNAME}" = $JobList[$i]
+            "{#JOBRESULT}" = $JobResult[$i]
+        }
+        [void]$List.Add($Obj)
+    }
+    $List
+}
+
+function get-CBBackupStatusDataStorage {
+    $storagediscovery="C:\Zabbix\discovery.storage.txt"
+    $data=Get-Content -Path $storagediscovery
+    $count = ($data -match ('^Name:') -replace ('^Name: ','')).count
+    $StorageList=$data -match ('^Name:') -replace ('^Name: ','')  
+    $StorageIds=$data -match ('^ID:') -replace ('^ID: ','')  
+    $StorageSpace=$data -match ('^Space') -replace ('^Space Used: ','')
+
+     $List = New-Object -TypeName System.Collections.ArrayList
+     for($i=0; $i -lt $count; $i++){
+        $Obj = [PSCustomObject]@{
+            "{#STORAGEID}" = $StorageIds[$i]
+            "{#STORAGENAME}" = $StorageList[$i]
+            "{#STORAGESPACE}" = $StorageSpace[$i]
+        }
+        [void]$List.Add($Obj)
+     }
+    $List
+} 
+
+if ($a -eq "job") {
+    $result=get-CBBackupStatusDataJob | Where-Object {$_."{#JOBID}" -eq $b} |select-object ("{#JOBRESULT}")
+    $result.'{#JOBRESULT}'
+        }
+        if ($a -eq "storage") {
+            $result=get-CBBackupStatusDataStorage | Where-Object {$_."{#STORAGEID}" -eq $c} | Select-Object '{#STORAGESPACE}'
+            $result.'{#STORAGESPACE}'
+        }
---
+++ Templates/Windows/App-Cloudberry/cloudberry-discovery.ps1
@@ -0,0 +1,55 @@
+﻿Param(
+  [string]$select
+)
+
+function get-CBBackupDataJob {
+set-location "C:\Program Files\CloudBerryLab\CloudBerry Backup"
+$jobdiscovery='C:\Zabbix\discovery.job.txt'
+    #Coleta dados do Cloudberry puros
+    (./cbb.exe plan -list)[2..9999] -replace('^[1-999]\.','') -replace('^ ','') -replace('^  ','') -replace("'",'') | Out-File $jobdiscovery -Force utf8
+    $data=Get-Content -Path $jobdiscovery
+    $count = ($data -match ('^Name:') -replace ('^Name: ','')).count
+    $JobList=$data -match ('^Name:') -replace ('^Name: ','')  
+    $JobIds=$data -match ('^ID:') -replace ('^ID: ','')  
+    $JobResult=$data -match ('^Last Result:') -replace ('^Last Result: ','')
+    $List = New-Object -TypeName System.Collections.ArrayList
+    for($i=0; $i -lt $count; $i++){
+        $Obj = [PSCustomObject]@{
+            "{#JOBID}" = $JobIds[$i]
+            "{#JOBNAME}" = $JobList[$i]
+            "{#JOBRESULT}" = $JobResult[$i]
+        }
+        [void]$List.Add($Obj)
+    }
+    $List
+}
+
+function get-CBBackupDataStorage {
+set-location "C:\Program Files\CloudBerryLab\CloudBerry Backup"
+$storagediscovery='C:\Zabbix\discovery.storage.txt'
+    #Coleta dados do Cloudberry puros
+    (./cbb.exe account -list)[2..999] -replace('[1-999]\. ','')  -replace('\. ',"`n") -replace("'",'') -replace(" Space","`nSpace") | out-file -FilePath $storagediscovery -Encoding utf8 -Force
+    $data=Get-Content -Path $storagediscovery
+    $count=(($data) -match('^Name:')).Length
+    $StorageList=$data -match ('^Name:') -replace ('^Name: ','')        
+    $StorageIDs=$data -match ('^ID:') -replace ('^ID: ','')  
+    $StorageSpace=$data -match ('^Space') -replace ('^Space used: ','')    
+    $List = New-Object -TypeName System.Collections.ArrayList
+    for($i=0; $i -lt $count; $i++){
+        $Obj = [PSCustomObject]@{
+            "{#STORAGEID}" = $StorageIDs[$i]
+            "{#STORAGENAME}" = $StorageList[$i]
+            "{#STORAGESPACE}" = $StorageSpace[$i]
+        }
+        [void]$List.Add($Obj)
+    }
+    $List
+}
+
+if ($select -eq 'discovery.job') {
+    get-CBBackupDataJob | convertto-json
+    }if ($select -eq 'discovery.storage') {
+    Write-Output "["
+    get-CBBackupDataStorage | convertto-json
+    Write-Output "]"
+    }
